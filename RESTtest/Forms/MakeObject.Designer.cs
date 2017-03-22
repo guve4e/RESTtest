e1e573b61ae47431053325a521e409cb93b316dc
Val Kormanov <guve4e@google.com>
2017-03-22 16:03:45 -0500

    A-103 Auto Scroll fix in MakeObjects.cs

--- RESTtest/Forms/MakeObject.Designer.cs
+++ RESTtest/Forms/MakeObject.Designer.cs
@@ -45,6 +45,7 @@
             // 
             // flowLayoutPanel1
             // 
+            this.flowLayoutPanel1.AutoScroll = true;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(2, 12);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(350, 368);
